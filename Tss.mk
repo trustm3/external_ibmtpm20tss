8b830bad2b97641f3ad83927c424e4e47d2d7ca6
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2019-01-17 09:08:59 +0100

    Tss.mk: changes to build v1331

--- Tss.mk
+++ Tss.mk
@@ -9,45 +9,48 @@ common_cflags := \
         -ggdb -O0 -c \
         -DTPM_ENCRYPT_SESSIONS_DEFAULT="\"0\""
 
-tss_cflags := -DTPM_TSS -DTPM_POSIX -DTPM_SOCKET_UDS
+tss_cflags := -DTPM_TSS -DTPM_TPM20 -DTPM_POSIX -DTPM_SOCKET_UDS
 
 tss_c_includes := \
   external/ibmtpm20tss/utils/. \
-  external/ibmtpm20tss/utils/tss2 \
+  external/ibmtpm20tss/utils/ibmtss \
   external/ibmtpm20tss/src/. \
   external/openssl_legacy/include \
 
-
-tss_src_files := \
+tss_common_src_files := \
   utils/tss.c \
   utils/tssproperties.c \
-  utils/tssauth.c \
   utils/tssmarshal.c \
-  utils/tsscrypto.c \
+  utils/tssauth.c \
   utils/tssutils.c \
   utils/tsssocket.c \
   utils/tssdev.c \
   utils/tsstransmit.c \
   utils/tssresponsecode.c \
   utils/tssccattributes.c \
-  utils/fail.c \
-  utils/tssprint.c
+  utils/tssprint.c \
+  utils/Unmarshal.c \
+  utils/CommandAttributeData.c
 
-tpm_shared_src_files := \
-  src/Unmarshal.c \
-  src/Commands.c \
-  src/CommandAttributeData.c \
-  src/CpriHash.c \
-  src/CpriSym.c
+tss_common20_src_files := \
+  utils/tss20.c	\
+  utils/tssauth20.c \
+  utils/Commands.c \
+  utils/ntc2lib.c \
+  utils/tssntc.c
+
+tss_default_src_files := \
+  utils/tssfile.c \
+  utils/tsscryptoh.c \
+  utils/tsscrypto.c \
+  utils/tssprintcmd.c
 
 LOCAL_CFLAGS += $(tss_cflags) $(common_cflags)
 LOCAL_C_INCLUDES += $(tss_c_includes)
 
-LOCAL_LDFLAGS := -Wl
-
-LOCAL_SRC_FILES := $(tpm_shared_src_files) $(tss_src_files)
+LOCAL_SRC_FILES := $(tss_common_src_files) $(tss_common20_src_files) $(tss_default_src_files)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE:= libtss
+LOCAL_MODULE:= libibmtss
 
 include $(BUILD_STATIC_LIBRARY)
